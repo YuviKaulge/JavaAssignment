44f611b142f7c0d6da9dc4d4fe59334cf2afd9a7
YuviKaulge <100679348+YuviKaulge@users.noreply.github.com>
2022-06-26 19:04:04 +0530

    Add files via upload

---
+++ StringAssign/FindLength.java
@@ -0,0 +1,10 @@
+package StringAssign;
+
+public class FindLength {
+
+    public static void main(String[] args) {
+        String str = "Yuvraj Kaulge"; 
+	System.out.println(str.length());
+
+    }
+}
---
+++ StringAssign/TwoString.java
@@ -0,0 +1,12 @@
+package StringAssign;
+
+public class TwoString {
+
+    public static void main(String[] args) {
+        String str1 = "Yuvraj";
+		String str2 = " Kaulage ";
+		String str3 = str1.concat(str2);
+		System.out.println( str3);
+
+    }
+}
