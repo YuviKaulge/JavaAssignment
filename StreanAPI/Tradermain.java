4e39529aa25b81e50c6a3a58d151cc2254999b04
YuviKaulge <100679348+YuviKaulge@users.noreply.github.com>
2022-06-26 19:10:04 +0530

    Add files via upload

---
+++ StreanAPI/Tradermain.java
@@ -0,0 +1,34 @@
+package StreanAPI;
+
+import java.util.ArrayList; 
+import java.util.List;
+
+/**
+ * TraderMain
+ */
+public class Tradermain 
+{
+
+    public static void main(String[] args) 
+    {
+        List<Trader> list= new ArrayList<>();
+				list.add(new Trader("Iron Man", "Pune"));
+				list.add(new Trader("Thor", "Bangalore"));
+				list.add(new Trader("Hulk", "Pune"));
+				list.add(new Trader("Captain", "Delhi") );
+				list.add(new Trader("Shaktiman", "Ahmednagar") );
+
+                 
+
+                //Question 9
+		list.stream().map(x-> x.getCity()).distinct().forEach(System.out::println);
+        System.out.println();
+        
+        //Question 10
+		list.stream().filter(x->x.getCity().contains("Pune")).map(x->x.getName()).sorted()
+		.forEach(System.out::println);
+
+
+
+    }
+}
